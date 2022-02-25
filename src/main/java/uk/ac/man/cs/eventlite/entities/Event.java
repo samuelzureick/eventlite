9da88097ecc9652fd43620b862a628f2f092d88c
n00575sm <samuel.morris-3@student.manchester.ac.uk>
2022-02-25 11:47:43 +0000

    Add annotation for ManyToOne relation
    x83005sz, x72433ld, s19236yx, p69749jl

--- src/main/java/uk/ac/man/cs/eventlite/entities/Event.java
+++ src/main/java/uk/ac/man/cs/eventlite/entities/Event.java
@@ -7,6 +7,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -34,8 +36,9 @@ public class Event {
 	@Column()
 	private String name;
 
-	@Column()
-	private long venue;
+	@ManyToOne
+	@JoinColumn(name="venue")
+	private Venue venue;
 
 	public Event() {
 	}
@@ -72,11 +75,11 @@ public class Event {
 		this.name = name;
 	}
 
-	public long getVenue() {
+	public Venue getVenue() {
 		return venue;
 	}
-
-	public void setVenue(long venue) {
+	
+	public void setVenue(Venue venue) {
 		this.venue = venue;
 	}
 }
