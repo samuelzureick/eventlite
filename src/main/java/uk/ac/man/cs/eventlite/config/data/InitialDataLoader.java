0baff975eadd136c7aac85a3139c2523d5510654
Yanze Xin <yanze.xin@student.manchester.ac.uk>
2022-03-03 17:17:46 +0000

    Avoid loading initial data multiple times

--- src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
@@ -30,55 +30,59 @@ public class InitialDataLoader {
 
 	@Autowired
 	private VenueService venueService;
-	
-	@Autowired
-	private EventRepository eventRepository;
-	
-	@Autowired
-	private VenueRepository venueRepository;
 
 	@Bean
 	CommandLineRunner initDatabase() {
 		return args -> {
-            // Build and save initial venues here.
-            Venue venue = new Venue();
-            venue.setId(1);
-            venue.setName("Venue 1");
-            venue.setCapacity(100);
-            venueService.save(venue);
-            Venue venue2 = new Venue();
-            venue2.setId(2);
-            venue2.setName("Venue 2");
-            venue2.setCapacity(200);
-            venueService.save(venue2);
-            Venue venue3 = new Venue();
-            venue3.setId(3);
-            venue3.setName("Venue 3");
-            venue3.setCapacity(300);
-            venueService.save(venue3);
+			Venue venue = null, venue2 = null, venue3 = null;
+			
+			if (venueService.count() > 0) {
+				log.info("Database already populated with venues. Skipping venue initialization.");
+			} else {
+	            // Build and save initial venues here.
+	            venue = new Venue();
+	            venue.setId(1);
+	            venue.setName("Venue 1");
+	            venue.setCapacity(100);
+	            venueService.save(venue);
+	            venue2 = new Venue();
+	            venue2.setId(2);
+	            venue2.setName("Venue 2");
+	            venue2.setCapacity(200);
+	            venueService.save(venue2);
+	            venue3 = new Venue();
+	            venue3.setId(3);
+	            venue3.setName("Venue 3");
+	            venue3.setCapacity(300);
+	            venueService.save(venue3);
+			}
             
-            // Build and save initial events here.
-            Event event1 = new Event();
-            event1.setId(1);
-            event1.setName("Event 1");
-            event1.setVenue(venue);
-            event1.setTime(LocalTime.now());
-            event1.setDate(LocalDate.now());
-            eventService.save(event1);
-            Event event2 = new Event();
-            event2.setId(2);
-            event2.setName("Event 2");
-            event2.setVenue(venue2);
-            event2.setTime(LocalTime.now().plusHours(1));
-            event2.setDate(LocalDate.now().plusDays(1));
-            eventService.save(event2);
-            Event event3 = new Event();
-            event3.setId(3);
-            event3.setName("Event 3");
-            event3.setVenue(venue3);
-            event3.setTime(LocalTime.now().plusHours(2));
-            event3.setDate(LocalDate.now().plusDays(2));
-            eventService.save(event3);
+			if (eventService.count() > 0) {
+				log.info("Database already populated with events. Skipping event initialization.");
+			} else {
+	            // Build and save initial events here.
+	            Event event1 = new Event();
+	            event1.setId(1);
+	            event1.setName("Event 1");
+	            event1.setVenue(venue);
+	            event1.setTime(LocalTime.now());
+	            event1.setDate(LocalDate.now());
+	            eventService.save(event1);
+	            Event event2 = new Event();
+	            event2.setId(2);
+	            event2.setName("Event 2");
+	            event2.setVenue(venue2);
+	            event2.setTime(LocalTime.now().plusHours(1));
+	            event2.setDate(LocalDate.now().plusDays(1));
+	            eventService.save(event2);
+	            Event event3 = new Event();
+	            event3.setId(3);
+	            event3.setName("Event 3");
+	            event3.setVenue(venue3);
+	            event3.setTime(LocalTime.now().plusHours(2));
+	            event3.setDate(LocalDate.now().plusDays(2));
+	            eventService.save(event3);
+			}
         };
 	}
 }
