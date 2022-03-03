28c45cbaf8ac19e7f8e4621dd28a1ae0ecf2db7d
Luke Deaves-Jarvis <luke.deaves-jarvis@student.manchester.ac.uk>
2022-03-03 10:39:27 +0000

    Fixes failing EventsControllerAPIIntegration test
    Populates the test data as the data that was originally
    in the JSON files before we edited any code

--- src/test/java/uk/ac/man/cs/eventlite/config/data/TestDataLoader.java
+++ src/test/java/uk/ac/man/cs/eventlite/config/data/TestDataLoader.java
@@ -8,8 +8,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import uk.ac.man.cs.eventlite.dao.EventRepository;
 import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.dao.VenueRepository;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
 
 @Configuration
 @Profile("test")
@@ -22,13 +26,47 @@ public class TestDataLoader {
 
 	@Autowired
 	private VenueService venueService;
+	
+	@Autowired
+	private EventRepository eventRepository;
+	
+	@Autowired
+	private VenueRepository venueRepository;
 
 	@Bean
 	CommandLineRunner initDatabase() {
 		return args -> {
-			// Build and save test events and venues here.
-			// The test database is configured to reside in memory, so must be initialized
-			// every time.
-		};
-	}
-}
+			if (venueService.count() > 0) {
+				log.info("Database already populated with venues. Skipping venue initialization.");
+			} else {
+				Venue testVenue1 = new Venue();
+				testVenue1.setName("Kilburn Building");
+				testVenue1.setCapacity(120);
+				venueRepository.save(testVenue1);
+				Venue testVenue2 = new Venue();
+				testVenue2.setName("Online");
+				testVenue2.setCapacity(100000);
+				venueRepository.save(testVenue2);
+			}
+
+			if (eventService.count() > 0) {
+				log.info("Database already populated with events. Skipping event initialization.");
+			} else {
+				Event testEvent1 = new Event();
+				testEvent1.setName("COMP23412 Showcase, group F");
+				testEvent1.setVenue(1);
+				eventRepository.save(testEvent1);
+				
+				Event testEvent2 = new Event();
+				testEvent2.setName("COMP23412 Showcase, group G");
+				testEvent2.setVenue(1);
+				eventRepository.save(testEvent2);
+
+				Event testEvent3 = new Event();
+				testEvent3.setName("COMP23412 Showcase, group H");
+				testEvent3.setVenue(1);
+				eventRepository.save(testEvent3);
+
+		}
+	};
+}}
