2103d146baf634403127565a23274b37b86bf4fe
Guan Yu Soh <guan.soh@student.manchester.ac.uk>
2022-03-04 12:01:42 +0000

    Add findAllOrderByDateTime method
    Added findAllOrderByDateTime method in EventServiceImp
    and EventService, edited findAll to use
    findAllOrderByDateTime.
    
    Issue: #6

--- src/main/java/uk/ac/man/cs/eventlite/dao/EventServiceImpl.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/EventServiceImpl.java
@@ -1,5 +1,11 @@
 package uk.ac.man.cs.eventlite.dao;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.ac.man.cs.eventlite.entities.Event;
@@ -17,11 +23,28 @@ public class EventServiceImpl implements EventService {
 
 	@Override
 	public Iterable<Event> findAll() {
-		return eventRepository.findAll(); 
+		return findAllByOrderByDateTime(); 
 	}
 	
 	@Override
 	public void save(Event event) {
 		eventRepository.save(event);
 	}
+	
+	@Override
+	public Iterable<Event> findAllByOrderByDateTime(){
+		Iterable<Event> events = eventRepository.findAll();
+		//Convert to List
+		List<Event> list = StreamSupport
+				  .stream(events.spliterator(), false)
+				  .collect(Collectors.toList());
+		//Sort List
+		list.sort((a, b)
+					-> (a.getDate().compareTo(b.getDate()) == 0 ?
+							a.getTime().compareTo(b.getTime()) : 
+								a.getDate().compareTo(b.getDate())));
+		return list;
+		
+	}
+	
 }
