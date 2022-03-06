c00909c35716906e99fe2aaa57616d6d5702e9d9
n00575sm <samuel.morris-3@student.manchester.ac.uk>
2022-03-06 19:43:43 +0000

    Add back-end for adding a event

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -1,21 +1,29 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
 import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
 import uk.ac.man.cs.eventlite.exceptions.EventNotFoundException;
 
 @Controller
@@ -54,6 +62,22 @@ public class EventsController {
 		model.addAttribute("venues", venueService.findAll());
 
 		return "events/new";
+	}
+	
+	@PostMapping("/new")
+	public String createEvent(@ModelAttribute Event event, BindingResult errors,
+			Model model, RedirectAttributes redirectAttrs, @RequestParam("venue") String venue) {
+
+		if (errors.hasErrors()) {
+			model.addAttribute("event", event);
+			model.addAttribute("venues", venueService.findAll());
+			return "events/new";
+		}
+
+		eventService.save(event);
+		redirectAttrs.addFlashAttribute("ok_message", "New event added.");
+
+		return "redirect:/events";
 	}
 
 	@GetMapping
