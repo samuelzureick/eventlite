c2e63d14f50f2f555e1054edec34bd586601970d
Yanze Xin <yanze.xin@student.manchester.ac.uk>
2022-03-24 18:07:44 +0000

    Start writing tests

--- src/test/java/uk/ac/man/cs/eventlite/dao/VenueServiceTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/dao/VenueServiceTest.java
@@ -1,6 +1,12 @@
 package uk.ac.man.cs.eventlite.dao;
 
-import org.junit.jupiter.api.Disabled;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,17 +16,38 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
 import uk.ac.man.cs.eventlite.EventLite;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class)
 @DirtiesContext
 @ActiveProfiles("test")
-@Disabled
 public class VenueServiceTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 	@Autowired
 	private VenueService venueService;
 
-	// This class is here as a starter for testing any custom methods within the
-	// VenueService. Note: It is currently @Disabled!
+//	@BeforeAll
+//	public void initialise() {
+//		Venue venue = new Venue();
+//        venue.setName("Venue For Test");
+//        venue.setAddress("Stuart Road WA15 8QY");
+//        venue.setCapacity(1250);
+//        venueService.save(venue);
+//        Event event = new Event();
+//        event.setName("Event For Test");
+//        event.setVenue(venue);
+//        event.setTime(LocalTime.now());
+//        event.setDate(LocalDate.now());
+//        event.setDescription("This is just an event for test.");
+//	}
+	
+	@Test
+	public void countVenueTest() {
+		assertEquals(venueService.count(), 3);
+		return;
+	}
+
+
 }
