374c90aabc6c26705986acb519bf424d96e8ebc9
Yanze Xin <yanze.xin@student.manchester.ac.uk>
2022-05-04 22:27:57 +0100

    Migrate and build basic VenuesControllerTest

---
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
@@ -0,0 +1,72 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import uk.ac.man.cs.eventlite.config.Security;
+import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Venue;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(VenuesController.class)
+@Import(Security.class)
+public class VenuesControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Mock
+	private Venue venue;
+
+	@MockBean
+	private EventService eventService;
+
+	@MockBean
+	private VenueService venueService;
+
+	@Test
+	public void getIndexWhenNoVenues() throws Exception {
+		when(venueService.findAll()).thenReturn(Collections.<Venue>emptyList());
+
+		mvc.perform(get("/venues").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("venues/index")).andExpect(handler().methodName("getAllVenues"));
+
+		verify(venueService).findAll();
+		verifyNoInteractions(venue);
+	}
+
+	@Test
+	public void getIndexWithVenues() throws Exception {
+		when(venueService.findAll()).thenReturn(Collections.<Venue>singletonList(venue));
+
+		mvc.perform(get("/venues").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("venues/index")).andExpect(handler().methodName("getAllVenues"));
+
+		verify(venueService).findAll();
+	}
+
+	@Test
+	public void getVenueNotFound() throws Exception {
+		mvc.perform(get("/venues/99").accept(MediaType.TEXT_HTML)).andExpect(status().isNotFound())
+				.andExpect(view().name("venues/not_found")).andExpect(handler().methodName("getVenue"));
+	}
+}
