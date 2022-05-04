6574deb8032a3d98c35d1f72434cf32f11baebae
Yanze Xin <yanze.xin@student.manchester.ac.uk>
2022-05-04 23:33:55 +0100

    Add get and create test for VenuesController

--- src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
@@ -1,17 +1,30 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -24,6 +37,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import uk.ac.man.cs.eventlite.config.Security;
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.entities.Venue;
 
 @ExtendWith(SpringExtension.class)
@@ -69,4 +83,61 @@ public class VenuesControllerTest {
 		mvc.perform(get("/venues/99").accept(MediaType.TEXT_HTML)).andExpect(status().isNotFound())
 				.andExpect(view().name("venues/not_found")).andExpect(handler().methodName("getVenue"));
 	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void getVenueFound() throws Exception {
+		when(venueService.findById(25)).thenReturn(Optional.of(venue));
+		when(eventService.findAll()).thenReturn(Collections.<Event>emptyList());
+		doNothing().when(venue).setEmpty(true);
+		when(eventService.splitEventFuture(any(List.class))).thenReturn(new ArrayList<Event>());
+
+		mvc.perform(get("/venues/25").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+			.andExpect(view().name("venues/details")).andExpect(handler().methodName("getVenue"))
+			.andExpect(model().hasNoErrors()).andExpect(model().attribute("venue", venue));
+
+		verify(venueService).findById(25);
+		verify(eventService).findAll();
+		verify(eventService).splitEventFuture(any(List.class));
+	}
+
+	@Test
+	public void newVenuePage() throws Exception {
+		mvc.perform(get("/venues/new").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+			.andExpect(view().name("venues/new")).andExpect(handler().methodName("newVenue"))
+			.andExpect(model().hasNoErrors());
+	}
+
+	@Test
+	public void createNewVenueSuccess() throws Exception {
+		ArgumentCaptor<Venue> arg = ArgumentCaptor.forClass(Venue.class);
+		doNothing().when(venueService).save(any(Venue.class));
+
+		mvc.perform(post("/venues/new").with(user("Sam").roles(Security.ORGANIZER_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("name", "venue")
+				.param("address", "23 Manchester Road E14 3BD")
+				.param("capacity", "10")		
+				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isFound())
+				.andExpect(view().name("redirect:/venues")).andExpect(model().hasNoErrors())
+				.andExpect(handler().methodName("createVenue")).andExpect(flash().attributeExists("ok_message"));
+
+		verify(venueService).save(arg.capture());
+		assertEquals("venue", arg.getValue().getName());
+	}
+
+	@Test
+	public void createNewVenueWithWrongAuthority() throws Exception {
+		ArgumentCaptor<Venue> arg = ArgumentCaptor.forClass(Venue.class);
+
+		mvc.perform(post("/venues/new").with(user("Sam").roles(Security.ATTENDEE_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("name", "venue")
+				.param("address", "23 Manchester Road E14 3BD")
+				.param("capacity", "10")			
+				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isForbidden());
+
+		verify(venueService, never()).save(arg.capture());
+	}
+
 }
