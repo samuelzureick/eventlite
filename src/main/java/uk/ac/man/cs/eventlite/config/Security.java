5a7f6ee318b44e8f0473e3dd028c28ceabab54cb
Yanze Xin <yanze.xin@student.manchester.ac.uk>
2022-05-05 22:39:34 +0100

    Add full venues controller tests

--- src/main/java/uk/ac/man/cs/eventlite/config/Security.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/Security.java
@@ -74,6 +74,6 @@ public class Security extends WebSecurityConfigurerAdapter {
 		UserDetails ruben = User.withUsername("Ruben").password(encoder.encode("Secret")).roles(ORGANIZER_ROLE).build();
 		UserDetails ryan = User.withUsername("Ryan").password(encoder.encode("software")).roles(ORGANIZER_ROLE).build();
 		UserDetails attendee = User.withUsername("attendee").password(encoder.encode("ROLE")).roles(ATTENDEE_ROLE).build();
-		return new InMemoryUserDetailsManager(rob, caroline, markel, mustafa, tom, sam, ruben, ryan);
+		return new InMemoryUserDetailsManager(rob, caroline, markel, mustafa, tom, sam, ruben, ryan, attendee);
 	}
 }
--- src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
@@ -51,6 +52,9 @@ public class VenuesControllerTest {
 	@Mock
 	private Venue venue;
 
+	@Mock
+	private Event event;
+
 	@MockBean
 	private EventService eventService;
 
@@ -90,7 +94,7 @@ public class VenuesControllerTest {
 		when(venueService.findById(25)).thenReturn(Optional.of(venue));
 		when(eventService.findAll()).thenReturn(Collections.<Event>emptyList());
 		doNothing().when(venue).setEmpty(true);
-		when(eventService.splitEventFuture(any(List.class))).thenReturn(new ArrayList<Event>());
+		when(eventService.splitEventFuture(any(List.class))).thenReturn(Collections.<Event>emptyList());
 
 		mvc.perform(get("/venues/25").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
 			.andExpect(view().name("venues/details")).andExpect(handler().methodName("getVenue"))
@@ -113,11 +117,11 @@ public class VenuesControllerTest {
 		ArgumentCaptor<Venue> arg = ArgumentCaptor.forClass(Venue.class);
 		doNothing().when(venueService).save(any(Venue.class));
 
-		mvc.perform(post("/venues/new").with(user("Sam").roles(Security.ORGANIZER_ROLE))
+		mvc.perform(post("/venues/new").with(user("Ryan").roles(Security.ORGANIZER_ROLE))
 				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
 				.param("name", "venue")
 				.param("address", "23 Manchester Road E14 3BD")
-				.param("capacity", "10")		
+				.param("capacity", "10")
 				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isFound())
 				.andExpect(view().name("redirect:/venues")).andExpect(model().hasNoErrors())
 				.andExpect(handler().methodName("createVenue")).andExpect(flash().attributeExists("ok_message"));
@@ -130,14 +134,143 @@ public class VenuesControllerTest {
 	public void createNewVenueWithWrongAuthority() throws Exception {
 		ArgumentCaptor<Venue> arg = ArgumentCaptor.forClass(Venue.class);
 
-		mvc.perform(post("/venues/new").with(user("Sam").roles(Security.ATTENDEE_ROLE))
+		mvc.perform(post("/venues/new").with(user("attendee").roles(Security.ATTENDEE_ROLE))
 				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
 				.param("name", "venue")
 				.param("address", "23 Manchester Road E14 3BD")
-				.param("capacity", "10")			
+				.param("capacity", "10")
 				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isForbidden());
 
 		verify(venueService, never()).save(arg.capture());
+	}
+
+	@Test
+	public void deleteVenueSuccess() throws Exception {
+		when(venueService.findById(25)).thenReturn(Optional.of(venue));
+		when(eventService.findAll()).thenReturn(Collections.<Event>emptyList());
+		doNothing().when(venueService).deleteById(25);
+	
+		mvc.perform(delete("/venues/25").with(user("Ryan").roles(Security.ORGANIZER_ROLE))
+			.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isFound())
+			.andExpect(view().name("redirect:/venues")).andExpect(handler().methodName("deleteVenue"))
+			.andExpect(flash().attributeExists("ok_message"));
+		
+		verify(venueService).findById(25);
+		verify(venueService).deleteById(25);
+	}
+
+	@Test
+	public void deleteVenueThatDoesNotExist() throws Exception {
+		mvc.perform(delete("/venues/99").with(user("Ryan").roles(Security.ORGANIZER_ROLE))
+			.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isNotFound())
+			.andExpect(view().name("venues/not_found")).andExpect(handler().methodName("deleteVenue"));
+
+		verify(venueService, never()).deleteById(99);
+	}
+
+	@Test
+	public void deleteVenueThatHasEvent() throws Exception {
+		when(venueService.findById(25)).thenReturn(Optional.of(venue));
+		List<Event> events = new ArrayList<Event>();
+		events.add(event);
+		when(eventService.findAll()).thenReturn(events);
+		when(event.getVenue()).thenReturn(venue);
+		when(venue.getId()).thenReturn(25l);
+	
+		mvc.perform(delete("/venues/25").with(user("Ryan").roles(Security.ORGANIZER_ROLE))
+			.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isFound())
+			.andExpect(view().name("redirect:/venues/25")).andExpect(handler().methodName("deleteVenue"));
+		
+		verify(venueService).findById(25);
+		verify(eventService).findAll();
+		verify(event).getVenue();
+		verify(venue).getId();
+	}
+
+	@Test
+	public void deleteVenueWithWrongAuthority() throws Exception {
+		mvc.perform(delete("/venues/25").with(user("attendee").roles(Security.ATTENDEE_ROLE))
+				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isForbidden());
+		
+		verify(eventService, never()).findById(25);
+		verify(eventService, never()).deleteById(25);
+	}
+
+	@Test
+	public void updateVenuePage() throws Exception {
+		when(venueService.findById(25)).thenReturn(Optional.of(venue));
+		when(venueService.findAll()).thenReturn(Collections.<Venue>emptyList());
+
+		mvc.perform(get("/venues/update/25").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+			.andExpect(view().name("venues/update")).andExpect(handler().methodName("getVenueUpdate"))
+			.andExpect(model().hasNoErrors());
+
+		verify(venueService).findById(25);
+		verify(venueService).findAll();
+	}
+
+	@Test
+	public void updateVenueSuccess() throws Exception {
+		ArgumentCaptor<Venue> arg = ArgumentCaptor.forClass(Venue.class);
+		when(venueService.findById(25)).thenReturn(Optional.of(venue));
+		doNothing().when(venueService).save(any(Venue.class));
+
+		mvc.perform(post("/venues/update").with(user("Ryan").roles(Security.ORGANIZER_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("id", "25")
+				.param("name", "venue")
+				.param("address", "23 Manchester Road E14 3BD")
+				.param("capacity", "10")		
+				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isFound())
+				.andExpect(view().name("redirect:/venues/25")).andExpect(model().hasNoErrors())
+				.andExpect(handler().methodName("updateVenue")).andExpect(flash().attributeExists("ok_message"));
+
+		verify(venueService).save(arg.capture());
+	}
+
+	@Test
+	public void updateVenueWithErrors() throws Exception {
+		ArgumentCaptor<Venue> arg = ArgumentCaptor.forClass(Venue.class);
+
+		mvc.perform(post("/venues/update").with(user("Ryan").roles(Security.ORGANIZER_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("id", "")  // Errors because we must state which venue to update
+				.param("name", "venue")
+				.param("address", "23 Manchester Road E14 3BD")
+				.param("capacity", "10")		
+				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isOk())
+				.andExpect(view().name("/venues/update")).andExpect(model().hasErrors())
+				.andExpect(handler().methodName("updateVenue"));
+
+		verify(venueService, never()).save(arg.capture());
+	}
+
+	@Test
+	public void updateVenueWithWrongAuthority() throws Exception {
+		ArgumentCaptor<Venue> arg = ArgumentCaptor.forClass(Venue.class);
+
+		mvc.perform(post("/venues/update").with(user("attendee").roles(Security.ATTENDEE_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("name", "venue")
+				.param("address", "23 Manchester Road E14 3BD")
+				.param("capacity", "10")
+				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isForbidden());
+
+		verify(venueService, never()).save(arg.capture());
+	}
+
+	@Test
+	public void searchVenue() throws Exception {
+		when(venueService.listAll("search_venue")).thenReturn(Collections.<Venue>emptyList());
+
+		mvc.perform(get("/venues/search")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("keyword", "search_venue")
+				.accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("venues/index")).andExpect(model().hasNoErrors())
+				.andExpect(handler().methodName("getSearchVenues"));
+
+		verify(venueService).listAll("search_venue");
 	}
 
 }
