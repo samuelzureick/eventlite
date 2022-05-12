3345d9c6391cdd646256b2fa8823c71f59927fa9
Yanze Xin <yanze.xin@student.manchester.ac.uk>
2022-05-12 20:43:53 +0100

    Add events controller integration tests

--- src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerIntegrationTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerIntegrationTest.java
@@ -1,7 +1,14 @@
 package uk.ac.man.cs.eventlite.controllers;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.StringContains.containsString;
+import static org.hamcrest.core.StringEndsWith.endsWith;
+import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
+
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,7 +21,10 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import uk.ac.man.cs.eventlite.EventLite;
 
@@ -23,6 +33,9 @@ import uk.ac.man.cs.eventlite.EventLite;
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 @ActiveProfiles("test")
 public class EventsControllerIntegrationTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+	private static Pattern CSRF = Pattern.compile("(?s).*name=\"_csrf\".*?value=\"([^\"]+).*");
+	private static String SESSION_KEY = "JSESSIONID";
 
 	@LocalServerPort
 	private int port;
@@ -36,14 +49,139 @@ public class EventsControllerIntegrationTest extends AbstractTransactionalJUnit4
 
 	@Test
 	public void testGetAllEvents() {
-		client.get().uri("/events").accept(MediaType.TEXT_HTML).exchange().expectStatus().isOk();
+		client.get().uri("/events").accept(MediaType.TEXT_HTML).exchange().expectStatus().isOk().expectHeader()
+				.contentTypeCompatibleWith(MediaType.TEXT_HTML).expectBody(String.class).consumeWith(result -> {
+					assertThat(result.getResponseBody(), containsString("Future Events"));
+					assertThat(result.getResponseBody(), containsString("Past Events"));
+				});
 	}
 
 	@Test
 	public void getEventNotFound() {
 		client.get().uri("/events/99").accept(MediaType.TEXT_HTML).exchange().expectStatus().isNotFound().expectHeader()
 				.contentTypeCompatibleWith(MediaType.TEXT_HTML).expectBody(String.class).consumeWith(result -> {
+					assertThat(result.getResponseBody(), containsString("Event not found"));
 					assertThat(result.getResponseBody(), containsString("99"));
 				});
 	}
+
+	@Test
+	public void getEventFound() {
+		client.get().uri("/events/4").accept(MediaType.TEXT_HTML).exchange().expectStatus().isOk().expectHeader()
+				.contentTypeCompatibleWith(MediaType.TEXT_HTML).expectBody(String.class).consumeWith(result -> {
+					assertThat(result.getResponseBody(), containsString("Event Detail"));
+					assertThat(result.getResponseBody(), containsString("Event Alpha"));
+				});
+	}
+
+	@Test
+	public void getNewEventPage() {
+		client.get().uri("/events/new").accept(MediaType.TEXT_HTML).exchange().expectStatus().isOk().expectHeader()
+				.contentTypeCompatibleWith(MediaType.TEXT_HTML).expectBody(String.class).consumeWith(result -> {
+					assertThat(result.getResponseBody(), containsString("Add a new event"));
+				});
+	}
+
+	@Test
+	public void createNewEvent() {
+		String[] tokens = login();
+
+		MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+		form.add("_csrf", tokens[0]);
+		form.add("name", "event");
+		form.add("description", "this is an test event");
+		form.add("venue", "1");
+		form.add("date", "2022-06-10");
+		form.add("time", "23:17");
+
+		client.post().uri("/events/new").accept(MediaType.TEXT_HTML).contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.bodyValue(form).cookies(cookies -> {
+					cookies.add(SESSION_KEY, tokens[1]);
+				}).exchange().expectStatus().isFound().expectHeader().value("Location", endsWith("/events/"));
+	}
+
+	@Test
+	public void deleteEvent() {
+		String[] tokens = login();
+
+		MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+		form.add("_csrf", tokens[0]);
+		form.add("_method", "delete");
+
+		client.post().uri("/events/5").accept(MediaType.TEXT_HTML).contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.bodyValue(form).cookies(cookies -> {
+					cookies.add(SESSION_KEY, tokens[1]);
+				}).exchange().expectStatus().isFound().expectHeader().value("Location", endsWith("/events/"));
+	}
+
+	@Test
+	public void getEventUpdatePage() {
+		client.get().uri("/events/update/6").accept(MediaType.TEXT_HTML).exchange().expectStatus().isOk().expectHeader()
+				.contentTypeCompatibleWith(MediaType.TEXT_HTML).expectBody(String.class).consumeWith(result -> {
+					assertThat(result.getResponseBody(), containsString("Update Event"));
+					assertThat(result.getResponseBody(), containsString("Event Apple"));
+				});
+	}
+
+	@Test
+	public void updateEvent() {
+		String[] tokens = login();
+
+		MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+		form.add("_csrf", tokens[0]);
+		form.add("id", "6");
+		form.add("name", "event updated");
+		form.add("description", "this is an test event");
+		form.add("venue", "1");
+		form.add("date", "2022-06-10");
+		form.add("time", "23:17");
+
+		client.post().uri("/events/update").accept(MediaType.TEXT_HTML).contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.bodyValue(form).cookies(cookies -> {
+					cookies.add(SESSION_KEY, tokens[1]);
+				}).exchange().expectStatus().isFound().expectHeader().value("Location", endsWith("/events/6/"));
+	}
+
+	@Test
+	public void searchEvent() {
+		client.get().uri("/events/search/?keyword=event").accept(MediaType.TEXT_HTML).exchange().expectStatus().isOk().expectHeader()
+				.contentTypeCompatibleWith(MediaType.TEXT_HTML).expectBody(String.class).consumeWith(result -> {
+					assertThat(result.getResponseBody(), containsString("Future Events"));
+					assertThat(result.getResponseBody(), containsString("Past Events"));
+				});
+	}
+
+	@Test
+	public void shareEvent() {
+		String[] tokens = login();
+
+		MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+		form.add("_csrf", tokens[0]);
+		form.add("text", "Integration Test " + new Date().toString());
+
+		client.post().uri("/events/4/share").accept(MediaType.TEXT_HTML).contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.bodyValue(form).cookies(cookies -> {
+					cookies.add(SESSION_KEY, tokens[1]);
+				}).exchange().expectStatus().isFound().expectHeader().value("Location", containsString("/events/4/"));
+	}
+
+	private String[] login() {
+		String[] tokens = new String[2];
+
+		EntityExchangeResult<String> result = client.mutate().filter(basicAuthentication("Ryan", "software")).build().get()
+				.uri("/events").accept(MediaType.TEXT_HTML).exchange().expectBody(String.class).returnResult();
+		tokens[0] = getCsrfToken(result.getResponseBody());
+		tokens[1] = result.getResponseCookies().getFirst(SESSION_KEY).getValue();
+
+		return tokens;
+	}
+
+	private String getCsrfToken(String body) {
+		Matcher matcher = CSRF.matcher(body);
+
+		assertThat(matcher.matches(), equalTo(true));
+
+		return matcher.group(1);
+	}
+
 }
